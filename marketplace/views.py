b34529fbca4f47365aabaa3402b8e1805b2cc211
dev-karan21K <kn5271443@gmail.com>
2025-12-29 18:16:39 +0530

    openinghour feature

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from vendor.models import Vendor
+from vendor.models import Vendor, OpeningHour
 from menu.models import Category, FoodItem
 from django.db.models import Prefetch
 from django.http import HttpResponse
@@ -11,6 +11,7 @@ from django.db.models import Q
 from django.contrib.gis.geos import GEOSGeometry
 from django.contrib.gis.measure import D
 from django.contrib.gis.db.models.functions import Distance
+from datetime import date, datetime
 
 # Create your views here.
 def marketplace(request):
@@ -33,6 +34,14 @@ def vendor_detail(request, vendor_slug):
         )
     )
 
+    opening_hours = OpeningHour.objects.filter(vendor=vendor).order_by('day','-from_hour')
+
+    # Check current day's opening hours
+    today_date = date.today()
+    today = today_date.isoweekday()
+
+    current_opening_hours = OpeningHour.objects.filter(vendor=vendor, day=today)
+
     if request.user.is_authenticated:
         cart_items = Cart.objects.filter(user=request.user)
     else:
@@ -41,7 +50,9 @@ def vendor_detail(request, vendor_slug):
     context = {
         'vendor': vendor,
         'categories': categories,
-        'cart_items': cart_items
+        'cart_items': cart_items,
+        'opening_hours': opening_hours,
+        'current_opening_hours': current_opening_hours,
     }
     return render(request, 'marketplace/vendor_detail.html', context)
 
